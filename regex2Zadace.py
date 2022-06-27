e004a0affeb966dc7ddeb5c678b51e16c90343ad
LekoIvana <108306653+LekoIvana@users.noreply.github.com>
2022-06-28 00:32:07 +0200

    Add files via upload

---
+++ regex2Zadace.py
@@ -0,0 +1,13 @@
+import re
+ 
+regex = "^i[0-5]+\l.*l$"
+
+while True:
+    unos = input("unesite ime:")
+    provjera = re.match(regex,unos)
+    if provjera:
+        break
+    else:
+        print("pogresan unos!")
+
+print("uspjesno!")
